9717203849a3c7ef12a339a77c2940b68a34217b
Crimson17 <46162167+Krimzo@users.noreply.github.com>
2022-07-08 14:37:02 +0200

    klib return

---
+++ KLibrary/utility/async.cpp
@@ -0,0 +1,22 @@
+#include "utility/async.h"
+
+
+void kl::async::loop(int64 startInclusive, int64 endExclusive, const std::function<void(uint t, int64 i)>& loopBody, uint threadCount) {
+	std::vector<std::thread> workers(threadCount);
+
+	std::atomic<int64> workCounter = startInclusive;
+	for (uint t = 0; t < threadCount; t++) {
+		workers[t] = std::thread([&, t]() {
+			int64 workIndex = startInclusive;
+			do {
+				workIndex = workCounter++;
+				loopBody(t, workIndex);
+			}
+			while (workIndex < endExclusive);
+		});
+	}
+
+	for (auto& worker : workers) {
+		worker.join();
+	}
+}
