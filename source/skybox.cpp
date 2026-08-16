e8df2ad5f2068486b6b2ea67accef30b0ee94c3a
Ivan P. <46162167+Krimzo@users.noreply.github.com>
2026-08-16 22:44:06 +0200

    fix skybox loading

--- source/skybox.cpp
+++ source/skybox.cpp
@@ -41,46 +41,31 @@ void ConguessSkybox::update()
 void load_box_texture( kl::GPU& gpu, std::string_view const& path, kl::dx::ShaderView& out_sv )
 {
     log( "Loading cube image ", path );
-    const kl::Image image{ path };
-    if ( image.pixel_count() == 0 )
+    kl::Texture box_texture{ gpu };
+    box_texture.image.load_from_file( path );
+    if ( box_texture.image.pixel_count() == 0 )
     {
         log_error( "Failed to load cube image ", path );
         return;
     }
-    if ( image.width() % 4 != 0 || image.height() % 3 != 0 )
+    if ( box_texture.image.width() % 4 != 0 || box_texture.image.height() % 3 != 0 )
     {
         log_error( "Cube image ", path, " has unsupported ratio" );
         return;
     }
-
-    const int part_width = image.width() / 4;
-    const int part_height = image.height() / 3;
-    if ( part_width != part_height )
-    {
-        log_error( "Cube image ", path, " width part and height part are not the same" );
-        return;
-    }
-
-    const kl::Int2 part_size{ part_width, part_height };
-    const kl::Image front = image.rectangle( part_size * kl::Int2( 1, 1 ), part_size * kl::Int2( 2, 2 ) );
-    const kl::Image back = image.rectangle( part_size * kl::Int2( 3, 1 ), part_size * kl::Int2( 4, 2 ) );
-    const kl::Image left = image.rectangle( part_size * kl::Int2( 0, 1 ), part_size * kl::Int2( 1, 2 ) );
-    const kl::Image right = image.rectangle( part_size * kl::Int2( 2, 1 ), part_size * kl::Int2( 3, 2 ) );
-    const kl::Image top = image.rectangle( part_size * kl::Int2( 1, 0 ), part_size * kl::Int2( 2, 1 ) );
-    const kl::Image bottom = image.rectangle( part_size * kl::Int2( 1, 2 ), part_size * kl::Int2( 2, 3 ) );
     log( "Loading cube texture ", path );
-    const kl::dx::Texture cube_texture = gpu.create_cube_texture( front, back, left, right, top, bottom );
-    if ( !cube_texture )
+    if ( !box_texture.reload_as_cube() )
     {
         log_error( "Failed to load cube texture ", path );
         return;
     }
     log( "Creating cube shader view ", path );
-    out_sv = gpu.create_shader_view( cube_texture, {} );
-    if ( !out_sv )
+    box_texture.create_shader_view();
+    if ( !box_texture.shader_view )
     {
         log_error( "Failed to create cube shader view ", path );
         return;
     }
+    out_sv = box_texture.shader_view;
     log_success( "Cube texture ", path, " is good." );
 }
